0c0335feee37f99bd3c73076917f953b7145692f
Dang Van Thanh <dangvanthanh@dangthanh.org>
2023-06-10 11:30:06 +0700

    Use LiveView Component

--- cuisine/lib/cuisine_web/components/core_components.ex
+++ cuisine/lib/cuisine_web/components/core_components.ex
@@ -650,4 +650,10 @@ defmodule CuisineWeb.CoreComponents do
   def translate_errors(errors, field) when is_list(errors) do
     for {^field, {msg, opts}} <- errors, do: translate_error({msg, opts})
   end
+
+  def greet(assigns) do
+    ~H"""
+    <p><%= @name %>!</p>
+    """
+  end
 end
