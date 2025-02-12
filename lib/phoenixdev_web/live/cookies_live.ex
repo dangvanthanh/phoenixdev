98edd26871accff0117131ec64b8b35ef03f6009
Dang Van Thanh <dangvanthanh@dangthanh.org>
2025-02-12 11:39:39 +0700

    chore: add liveview cookies

---
+++ lib/phoenixdev_web/live/cookies_live.ex
@@ -0,0 +1,65 @@
+defmodule PhoenixdevWeb.CookiesLive do
+  use PhoenixdevWeb, :live_view
+
+  def render(assigns) do
+    # IO.puts("rendering")
+
+    ~H"""
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-purple-900 to-red-900 text-white">
+      <h1 class="absolute top-5 left-5 text-2xl font-bold">Cookie Clicker</h1>
+      <div class="text-6xl mb-4">{@count}</div>
+      <div
+        class="text-9xl cursor-pointer select-none transition-transform duration-100 hover:scale-110 active:scale-90"
+        phx-click="increment"
+        phx-value-amount="1"
+        phx-keydown="increment"
+        phx-key="Enter"
+        tabindex="0"
+      >
+        🍪
+      </div>
+      <div class="mt-10 space-x-4">
+        <button
+          type="button"
+          class="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700"
+          phx-click="increment"
+          phx-value-amount="1"
+        >
+          +1
+        </button>
+        <button
+          type="button"
+          class="bg-green-600 text-white py-2 px-4 rounded hover:bg-green-700"
+          phx-click="increment"
+          phx-value-amount="5"
+        >
+          +5
+        </button>
+        <button
+          type="button"
+          class="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700"
+          phx-click="increment"
+          phx-value-amount="10"
+        >
+          +10
+        </button>
+      </div>
+    </div>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    # if connected?(socket) do
+    #   IO.puts("mounting (connected)")
+    # else
+    #   IO.puts("mounting (not connected)")
+    # end
+
+    {:ok, assign(socket, count: 0)}
+  end
+
+  def handle_event("increment", %{"amount" => amount}, socket) do
+    new_count = socket.assigns.count + String.to_integer(amount)
+    {:noreply, assign(socket, :count, new_count)}
+  end
+end
--- lib/phoenixdev_web/router.ex
+++ lib/phoenixdev_web/router.ex
@@ -18,6 +18,7 @@ defmodule PhoenixdevWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    live "/cookies", CookiesLive
   end
 
   # Other scopes may use custom stacks.
